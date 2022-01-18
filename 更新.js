11965bf4bb942e57575dc0dc8de743dc4d63a239
lllrrr3 <75841879+lllrrr3@users.noreply.github.com>
2022-01-18 23:29:53 +0800

    Add files via upload

---
+++ 更新.js
@@ -0,0 +1,13 @@
+function github(githubfile, savefile) {
+    let url = "https://raw.fastgit.org/" + githubfile;
+    res = http.get(url);
+    if (res.statusCode != 200) {
+        toast("请求失败");
+    }
+    files.writeBytes("/sdcard/脚本/" + savefile, res.body.bytes());
+    toast("下载成功");
+}
+github("lllrrr3/jdns/main/助力.js", "助力.js");
+github("lllrrr3/jdns/main/助力1.js", "助力1.js");
+github("lllrrr3/jdns/main/助力2.js", "助力2.js");
+github("lllrrr3/jdns/main/炸年兽.js", "炸年兽.js");
