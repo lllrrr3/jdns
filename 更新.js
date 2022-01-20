b9b86172caaf27f49e9ee8a5e1fbdb83cc051b28
lllrrr3 <75841879+lllrrr3@users.noreply.github.com>
2022-01-20 19:57:04 +0800

    Update 更新.js

--- 更新.js
+++ 更新.js
@@ -10,3 +10,6 @@ function github(githubfile, savefile) {
 github("lllrrr3/jdns/main/助力1.js", "助力1.js");
 github("lllrrr3/jdns/main/助力2.js", "助力2.js");
 github("lllrrr3/jdns/main/炸年兽.js", "炸年兽.js");
+github("lllrrr3/jdns/main/main.js", "main.js");
+github("lllrrr3/jdns/main/zns2022.js", "zns2022.js");
+github("lllrrr3/jdns/main/2022年兽autojs.js", "2022年兽autojs.js");
